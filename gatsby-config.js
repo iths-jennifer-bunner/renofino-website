3db8ae7d31b931deb60e52b8d772ddb25f5a13bf
iths-jennifer-bunner <jenniferbunner@hotmail.com>
2024-05-17 10:09:22 +0200

    stylings and plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,5 +8,24 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  plugins: [],
-}
+  plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`, // Needed for dynamic images],
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        // The unique name for each instance
+        name: `pages`,
+        // Path to the directory
+        path: `${__dirname}/src/pages/`,
+      },
+    },
+  ],
+  siteMetadata: {
+    title: 'Ren & Fino',
+    description:
+      'Vi är städfirman med målsättningen att skapa livskvalitet med professionell städservice',
+    copyright: 'Copyright 2024 Jennifer Bunner',
+  },
+};
